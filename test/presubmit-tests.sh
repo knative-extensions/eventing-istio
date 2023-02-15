60afb618839256e3777c56b18e4003321d3ee4e1
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-02-15 14:43:53 +0100

    Add e2e tests setup
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- test/presubmit-tests.sh
+++ test/presubmit-tests.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
+source $(dirname $0)/../vendor/knative.dev/hack/presubmit-tests.sh
 source $(dirname $0)/e2e-common.sh
 
-if ! ${SKIP_INITIALIZE}; then
-  initialize $@ --skip-istio-addon --min-nodes 3 --max-nodes 3
-fi
-
-# TODO(approvers): replace with real tests
-true
+main $@
