a031a623e9c8d4439bf273b5a295cd77a1bdf661
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-02-16 13:02:26 +0100

    Run PingSource tests
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -3,12 +3,50 @@
 REPO_ROOT_DIR=$(dirname $0)/..
 
 export SKIP_INITIALIZE=${SKIP_INITIALIZE:-false}
+export SYSTEM_NAMESPACE=${SYSTEM_NAMESPACE:-"knative-eventing"}
 
-source $(pwd)/vendor/knative.dev/hack/e2e-tests.sh
+source "${REPO_ROOT_DIR}"/vendor/knative.dev/hack/e2e-tests.sh
 
 git submodule update --init --recursive
 
 function knative_setup() {
   "${REPO_ROOT_DIR}"/hack/update-istio.sh || return $?
   "${REPO_ROOT_DIR}"/hack/install-dependencies.sh || return $?
+  kubectl apply -Rf "${REPO_ROOT_DIR}"/test/config
+}
+
+function run_eventing_core_tests() {
+  pushd "${REPO_ROOT_DIR}"/third_party/eventing || return $?
+
+  go_test_e2e \
+    -timeout=1h \
+    -run TestPingSource \
+    ./test/rekt/... \
+    --istio.enabled=true || return $?
+
+  pod
+}
+
+function run_eventing_kafka_broker_tests() {
+  pushd "${REPO_ROOT_DIR}"/third_party/eventing-kafka-broker || return $?
+
+  BROKER_CLASS=Kafka go_test_e2e \
+    -timeout=1h \
+    -run TestBrokerConformance \
+    ./test/e2e_new/... \
+    --istio.enabled=true || return $?
+
+  BROKER_CLASS=KafkaNamespaced go_test_e2e \
+    -timeout=1h \
+    -run TestBrokerConformance \
+    ./test/e2e_new/... \
+    --istio.enabled=true || return $?
+
+  BROKER_CLASS=Kafka go_test_e2e \
+    -timeout=1h \
+    -run TestKafkaSource \
+    ./test/e2e_new/... \
+    --istio.enabled=true || return $?
+
+  pod
 }
