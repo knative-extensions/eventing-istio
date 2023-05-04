a7dfa7ca127cfb4e0ec363877f5b27cf7cbea669
Pierangelo Di Pilato <pierdipi@redhat.com>
2023-05-04 12:10:56 +0200

    Update codegen
    Signed-off-by: Pierangelo Di Pilato <pierdipi@redhat.com>

--- pkg/client/injection/kube/client/client_expansion.go
+++ pkg/client/injection/kube/client/client_expansion.go
@@ -5,7 +5,7 @@ Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
-	http://www.apache.org/licenses/LICENSE-2.0
+    http://www.apache.org/licenses/LICENSE-2.0
 
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
@@ -17,8 +17,8 @@ limitations under the License.
 package client
 
 import (
-	"context"
-	"fmt"
+	context "context"
+	fmt "fmt"
 
 	certificatesv1beta1 "k8s.io/api/certificates/v1beta1"
 	corev1 "k8s.io/api/core/v1"
