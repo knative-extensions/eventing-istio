da4f391dc68f2b292603a18ed5cb1138ac77cda6
Christoph Stäbler <cstabler@redhat.com>
2023-07-12 09:00:31 +0200

    Fix REPO_ROOT_DIR env override in e2e test
    Signed-off-by: Christoph Stäbler <cstabler@redhat.com>

--- test/e2e-common.sh
+++ test/e2e-common.sh
@@ -1,27 +1,27 @@
 #!/usr/bin/env bash
 
-REPO_ROOT_DIR=$(dirname $(realpath $0))/..
+repo_root_dir=$(dirname $(realpath $0))/..
 
-echo "$REPO_ROOT_DIR"
+echo "$repo_root_dir"
 
 export SKIP_INITIALIZE=${SKIP_INITIALIZE:-false}
 export SYSTEM_NAMESPACE=${SYSTEM_NAMESPACE:-"knative-eventing"}
 export ISTIO_NAMESPACE=${ISTIO_NAMESPACE:-"istio-system"}
-export KAFKA_BROKER_TEMPLATES=${KAFKA_BROKER_TEMPLATES:-"${REPO_ROOT_DIR}/test/e2e/templates/kafka-broker"}
-export KAFKA_NAMESPACED_BROKER_TEMPLATES=${KAFKA_BROKER_TEMPLATES:-"${REPO_ROOT_DIR}/test/e2e/templates/kafka-namespaced-broker"}
+export KAFKA_BROKER_TEMPLATES=${KAFKA_BROKER_TEMPLATES:-"${repo_root_dir}/test/e2e/templates/kafka-broker"}
+export KAFKA_NAMESPACED_BROKER_TEMPLATES=${KAFKA_BROKER_TEMPLATES:-"${repo_root_dir}/test/e2e/templates/kafka-namespaced-broker"}
 
-source "${REPO_ROOT_DIR}"/vendor/knative.dev/hack/e2e-tests.sh
+source "${repo_root_dir}"/vendor/knative.dev/hack/e2e-tests.sh
 
 function knative_setup() {
   git submodule update --init --recursive
-  "${REPO_ROOT_DIR}"/hack/install-dependencies.sh || return $?
-  "${REPO_ROOT_DIR}"/hack/install.sh || return $?
+  "${repo_root_dir}"/hack/install-dependencies.sh || return $?
+  "${repo_root_dir}"/hack/install.sh || return $?
 
   wait_until_pods_running "knative-eventing" || return $?
 }
 
 function run_eventing_core_tests() {
-  pushd "${REPO_ROOT_DIR}"/third_party/eventing || return $?
+  pushd "${repo_root_dir}"/third_party/eventing || return $?
 
   BROKER_TEMPLATES="${KAFKA_BROKER_TEMPLATES}" go_test_e2e \
     -timeout=1h \
@@ -80,7 +80,7 @@ function run_eventing_core_tests() {
 }
 
 function run_eventing_kafka_broker_tests() {
-  pushd "${REPO_ROOT_DIR}"/third_party/eventing-kafka-broker || return $?
+  pushd "${repo_root_dir}"/third_party/eventing-kafka-broker || return $?
 
   BROKER_TEMPLATES="${KAFKA_BROKER_TEMPLATES}" BROKER_CLASS="Kafka" go_test_e2e \
     -timeout=1h \
