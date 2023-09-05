85ccebc702d8242e1a0a8de4ae7067586a90a9be
Christoph Stäbler <cstabler@redhat.com>
2023-09-05 07:23:55 +0200

    Use new `num-nodes` parameter

--- test/e2e-tests.sh
+++ test/e2e-tests.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 source $(dirname $0)/e2e-common.sh
 
 if ! ${SKIP_INITIALIZE}; then
-  initialize $@ --skip-istio-addon --min-nodes 3 --max-nodes 3
+  initialize $@ --skip-istio-addon --num-nodes 3
 fi
 
 run_eventing_core_tests || fail_test
